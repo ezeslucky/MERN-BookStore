51d3d42d06f880203708582de2cc41ba139e2c71
Lucky <lucky123@gmail.com>
2024-09-23 02:28:50 +0530

    button

--- client/src/components/BackButton.jsx
+++ client/src/components/BackButton.jsx
@@ -3,10 +3,10 @@ import { BsArrowLeft } from 'react-icons/bs';
 
 const BackButton = ({ destination = '/' }) => {
   return (
-    <div className='flex'>
+    <div className='flex '>
       <Link
         to={destination}
-        className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
+        className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit hover:text-black hover:bg-sky-500'
       >
         <BsArrowLeft className='text-2xl' />
       </Link>
